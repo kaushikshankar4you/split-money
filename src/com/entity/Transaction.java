8f67b2ed59286a2e71140c59c67d1fa560469c09
Kaushik Shankar <kaushikshankar4you@gmail.com>
2020-05-16 23:00:22 +0530

    Transaction changes

--- src/com/entity/Transaction.java
+++ src/com/entity/Transaction.java
@@ -22,9 +22,7 @@ public class Transaction {
 	public int getUser_id() {
 		return user_id;
 	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
+	public void setUser_id(int user_id) { this.user_id = user_id; }
 	public int getProduct_id() {
 		return product_id;
 	}
